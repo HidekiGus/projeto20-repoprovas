397783bcdaa383e11bba97474cebbc700d4594a7
Gustavo <gustavohideki@outlook.com>
2022-09-15 01:46:37 -0300

    Feat: Initial setup for testing

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
-import express from 'express';
-import 'express-async-errors';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import app from './app';
 
 dotenv.config();
 
-import router from './routes';
-import errorHandler from './middlewares/errorHandler';
-
-const server = express();
-server.use(express.json());
-server.use(cors());
-server.use(router);
-server.use(errorHandler);
-
 const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
