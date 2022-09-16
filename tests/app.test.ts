7746f27e5689be5d9810a6e4d531edb181bcb04f
Gustavo <gustavohideki@outlook.com>
2022-09-16 03:19:58 -0300

    Feat: add sign up

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,13 @@
 import supertest from 'supertest';
 import { createUser } from './factories/userFactory';
 import app from '../src/app';
+import client from '../src/database/database';
+
+afterAll(() => {
+  afterAll(async () => {
+    await client.$disconnect();
+  });
+});
 
 describe('Testing user routes', () => {
   it('Returns 201 when a new user is created', async () => {
@@ -8,5 +15,13 @@ describe('Testing user routes', () => {
 
     const result = await supertest(app).post('/signup').send(newUser);
     expect(result.status).toEqual(201);
+  });
+
+  it('Returns 409 when email is already being used', async () => {
+    const newUser = await createUser();
+
+    await supertest(app).post('/signup').send(newUser);
+    const result = await supertest(app).post('/signup').send(newUser);
+    expect(result.status).toEqual(409);
   });
 });
